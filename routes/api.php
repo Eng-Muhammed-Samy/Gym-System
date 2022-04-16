f3ad3e484dfd8e64e1395efcda177abfdd51ee94
yasminekamal <yassminkamal92@gmail.com>
2022-04-16 07:34:43 -0700

    authorization

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GymMembersController;
+use App\Http\Controllers\AuthController;
 // use Google\Service\Storage;
 // use Illuminate\Filesystem\FilesystemManager as Storage;
 use App\Http\Controllers\Api\SessionController;
@@ -38,37 +39,52 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['prefix'=>'/gym-members','middleware'=>'auth:sanctum','middleware'=>'atLeastCityManager'],function () { 
 // gym member routes
-Route::prefix('/gym-members')->group(function () {
     Route::get('/', [GymMembersController::class, 'index']);
     Route::post('/store', [GymMembersController::class, 'store']);
     Route::post('/{id}/update', [GymMembersController::class, 'update']);
     Route::delete('/{id}/delete', [GymMembersController::class, 'destroy']);
-
 });
 //cityManager routes
 Route::resource('bans', BanController::class);
 Route::resource('users',UserController::class);
-Route::resource('citymanagers',CityMangerController::class)->except(['update']);
-Route::post('citymanagers/{id}/update',[CityMangerController::class,'update']);
-Route::get('cities-without-manager',[CityController::class,'withoutManager']);
-Route::resource('cities',CityController::class)->except(['update']);
-Route::post('citymanagers/{id}/update',[CityMangerController::class,'update']);
-Route::post('cities/{id}/update',[CityController::class,'update']);
 
-//Gym manager routes
-Route::resource('sessions', SessionController::class);
-Route::post('sessions/{id}/update',[SessionController::class,'update']);
-Route::resource('coaches', CoachController::class);
-Route::post('coaches/{id}/update',[CoachController::class,'update']);
-Route::resource('packages', PackageController::class);
-Route::resource('gymmanagers', GymManagerController::class)->except(['update']);
-Route::post('gymmanagers/{id}/update',[GymManagerController::class,'update']);
-Route::resource('attendances', AttendanceController::class);
 
-//Gym Route
-Route::resource('gyms', GymController::class);
-Route::post('gyms/{id}/update',[GymController::class,'update']);
+Route::group(['middleware'=>'auth:sanctum','middleware'=>'atLeastAdmin'],function () { 
+
+    Route::resource('citymanagers',CityMangerController::class)->except(['update']);
+    Route::post('citymanagers/{id}/update',[CityMangerController::class,'update']);
+    Route::get('cities-without-manager',[CityController::class,'withoutManager']);
+    Route::resource('cities',CityController::class)->except(['update']);
+    Route::post('citymanagers/{id}/update',[CityMangerController::class,'update']);
+    Route::post('cities/{id}/update',[CityController::class,'update']);
+
+});
+
+
+
+Route::group(['middleware'=>'auth:sanctum','middleware'=>'atLeastCityManager'],function () { 
+
+    Route::resource('gymmanagers', GymManagerController::class)->except(['update']);
+    Route::post('gymmanagers/{id}/update',[GymManagerController::class,'update']);
+    Route::resource('gyms', GymController::class);
+    Route::post('gyms/{id}/update',[GymController::class,'update']);
+
+});
+
+
+Route::group(['middleware'=>'auth:sanctum','middleware'=>'atLeastGymManager'],function () { 
+
+    Route::resource('sessions', SessionController::class);
+    Route::post('sessions/{id}/update',[SessionController::class,'update']);
+    Route::resource('coaches', CoachController::class);
+    Route::post('coaches/{id}/update',[CoachController::class,'update']);
+    Route::resource('packages', PackageController::class);
+    Route::resource('attendances', AttendanceController::class);
+
+});
+
 
 //Payment gateway routes
 Route::post('stripe', [StripeController::class, 'stripePost'])->name('stripe.post');
@@ -80,20 +96,5 @@ Route::post('stripe/createSubscription', [StripeController::class, 'createSubscr
 Route::post('stripe/createPaymentMethod', [StripeController::class, 'createPaymentMethod'])->name('stripe.paymentMethod');
 
 
-Route::post('/sanctum/token', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ]);
 
-    $user = User::where('email', $request->email)->first();
-
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    return $user->createToken($request->device_name)->plainTextToken;
-});
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
